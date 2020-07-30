9c207e73f08f3cb7e8aa1b6c15b53eb7bbb50c2f
simon-p-r <simon.ricaldone@circabs.com>
2020-07-30 15:54:14 +0100

    added strverscmp to versionsort function

--- include/dirent.h
+++ include/dirent.h
@@ -313,6 +313,19 @@ static int alphasort (const struct dirent **a, const struct dirent **b);
 
 static int versionsort (const struct dirent **a, const struct dirent **b);
 
+/* states: S_N: normal, S_I: comparing integral part, S_F: comparing
+           fractionnal parts, S_Z: idem but with leading Zeroes only */
+#define  S_N    0x0
+#define  S_I    0x3
+#define  S_F    0x6
+#define  S_Z    0x9
+
+/* result_type: CMP: return diff; LEN: compare using len_diff/diff */
+#define  CMP    2
+#define  LEN    3
+
+static int strverscmp (const char *a, const char* b);
+
 
 /* For compatibility with Symbian */
 #define wdirent _wdirent
@@ -1033,8 +1046,77 @@ static int
 versionsort(
     const struct dirent **a, const struct dirent **b)
 {
-    /* FIXME: implement strverscmp and use that */
-    return alphasort (a, b);
+    return strverscmp ((*a)->d_name, (*b)->d_name);
+}
+
+
+/* Sort versions */
+static int
+strverscmp(
+    const char *a, const char *b)
+{
+
+    const unsigned char *p1 = (const unsigned char *)a;
+    const unsigned char *p2 = (const unsigned char *)b;
+
+    /* Symbol(s)    0       [1-9]   others
+        Transition   (10) 0  (01) d  (00) x   */
+    static const unsigned char next_state[] = {
+        /* state    x    d    0  */
+        /* S_N */  S_N, S_I, S_Z,
+        /* S_I */  S_N, S_I, S_I,
+        /* S_F */  S_N, S_F, S_F,
+        /* S_Z */  S_N, S_F, S_Z
+    };
+
+    static const signed char result_type[] = {
+        /* state   x/x  x/d  x/0  d/x  d/d  d/0  0/x  0/d  0/0  */
+
+        /* S_N */  CMP, CMP, CMP, CMP, LEN, CMP, CMP, CMP, CMP,
+        /* S_I */  CMP, -1,  -1,  +1,  LEN, LEN, +1,  LEN, LEN,
+        /* S_F */  CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+        /* S_Z */  CMP, +1,  +1,  -1,  CMP, CMP, -1,  CMP, CMP
+    };
+
+    if (p1 == p2) {
+        return 0;
+    }
+
+    unsigned char c1 = *p1++;
+    unsigned char c2 = *p2++;
+    /* Hint: '0' is a digit too.  */
+    int state = S_N + ((c1 == '0') + (isdigit (c1) != 0));
+
+    int diff;
+    while ((diff = c1 - c2) == 0) {
+        if (c1 == '\0') {
+            return diff;
+        }
+
+        state = next_state[state];
+        c1 = *p1++;
+        c2 = *p2++;
+        state += (c1 == '0') + (isdigit (c1) != 0);
+    }
+
+    state = result_type[state * 3 + (((c2 == '0') + (isdigit (c2) != 0)))];
+
+    switch (state) {
+        case CMP:
+            return diff;
+
+        case LEN:
+            while (isdigit (*p1++)) {
+                if (!isdigit (*p2++)) {
+                    return 1;
+                }
+            }
+
+            return isdigit (*p2) ? -1 : diff;
+
+        default:
+            return state;
+    }
 }
 
 /* Convert multi-byte string to wide character string */
